7cc532e54ddb304d3efc813bf91fba1edd3642de
syam38 <syampradeep2b6@gmail.com>
2021-01-06 22:22:52 +0530

    linting and minor code refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,86 +1,75 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-matrix:number[][];
-winmatrix:number[][];
-  constructor(){
-  this.matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]];
-  this.winmatrix= [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]];
- }  
- checkForCompletion(){
-   var flag=0;
-   console.log(this.matrix);
-   console.log(this.winmatrix);
-   
-    for(let i=0;i<this.matrix.length;i++){    
-      for(let j=0;j<this.matrix[0].length;j++){
-        if(this.winmatrix[i][j]!=this.matrix[i][j]){
-            flag=1;
-        }
-     }
-    }
-    if(flag==0){
-      alert('You Won');
-    }
+  matrix: number[][] = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 0],
+  ];
+  winmatrix: number[][] = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 0],
+  ];
+  constructor() {
+  }
 
- }
- moveTile(tileNumber:number){  
-  var flag=0;
-  var won=1;
-  for(let i=0;i<this.matrix.length;i++){    
-      for(let j=0;j<this.matrix[0].length;j++){
-       if(this.matrix[i][j]==tileNumber){
-        this.swapTile(i,j);
-        this.checkForCompletion();
-        flag=1; 
-        break;       
+  checkForCompletion() {
+    let didNotWin;
+    for (let i = 0; i < this.matrix.length; i++) {
+      for (let j = 0; j < this.matrix[0].length; j++) {
+        if (this.winmatrix[i][j] != this.matrix[i][j]) {
+          didNotWin = true;
+          break;
+        }
       }
     }
-    if(flag==1){
-      break;
+    if(!didNotWin) {
+      alert('You Won');
     }
   }
- }
- swapTile(i:number,j:number){
-    if(i+1<this.matrix.length&&this.matrix[i+1][j]==0){                    
-              this.matrix[i+1][j]=this.matrix[i][j];              
-              this.matrix[i][j]=0;             
-          }
-          else if(j+1<this.matrix[0].length&&this.matrix[i][j+1]==0){
-              this.matrix[i][j+1]=this.matrix[i][j];
-              this.matrix[i][j]=0;
-             
-          }
-          else if(i-1>=0 && this.matrix[i-1][j]==0){
-              this.matrix[i-1][j]=this.matrix[i][j];
-              this.matrix[i][j]=0;
-             
-          }
-          else if(j-1>=0 && this.matrix[i][j-1]==0){
-              this.matrix[i][j-1]=this.matrix[i][j];
-              this.matrix[i][j]=0;
-              
-          }
-       
- }
 
-shuffleMatrix() {
-  var k,j,tempk,tempj;
-    for(var i = 0; i< this.matrix.length; i++) {
-       k = this.matrix[i].length;
-       while(k--){
-            j = Math.floor(Math.random() * (this.matrix.length - 1));
-            tempk = this.matrix[i][k];
-            tempj = this.matrix[i][j];
-            this.matrix[i][k] = tempj;
-            this.matrix[i][j] = tempk;
-       }
+  // move the tile to the empty space
+  moveTile(i, j) {
+    this.swapTile(i, j);
+    this.checkForCompletion();
+  }
+
+  // check the four sides of the tile to which it can be swapped
+  swapTile(i: number, j: number) {
+    if (i + 1 < this.matrix.length && this.matrix[i + 1][j] == 0) {
+      this.matrix[i + 1][j] = this.matrix[i][j];
+      this.matrix[i][j] = 0;
+    } else if (j + 1 < this.matrix[0].length && this.matrix[i][j + 1] == 0) {
+      this.matrix[i][j + 1] = this.matrix[i][j];
+      this.matrix[i][j] = 0;
+    } else if (i - 1 >= 0 && this.matrix[i - 1][j] == 0) {
+      this.matrix[i - 1][j] = this.matrix[i][j];
+      this.matrix[i][j] = 0;
+    } else if (j - 1 >= 0 && this.matrix[i][j - 1] == 0) {
+      this.matrix[i][j - 1] = this.matrix[i][j];
+      this.matrix[i][j] = 0;
     }
-}
-
+  }
+  
+  shuffleMatrix() {
+    let k, j, tempk, tempj;
+    for (let i = 0; i < this.matrix.length; i++) {
+      k = this.matrix[i].length;
+      while (k--) {
+        j = Math.floor(Math.random() * (this.matrix.length - 1));
+        tempk = this.matrix[i][k];
+        tempj = this.matrix[i][j];
+        this.matrix[i][k] = tempj;
+        this.matrix[i][j] = tempk;
+      }
+    }
+  }
 }
